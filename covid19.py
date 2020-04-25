7c77d7014a329379ce35f8e51eebcdcd5c8e2f2d
Aacgectyuoki <maximiliandellthib@mail.adelphi.edu>
2020-04-25 15:59:03 -0400

    Update covid19.py

--- covid19.py
+++ covid19.py
@@ -10,30 +10,68 @@ us_loc.tail()
 new_york = us_loc[us_loc["county"] == "New York City"]
 new_york.tail()
 
-# Create figure and plot space
-fig, ax = plt.subplots(figsize=(10, 10))
-# Add x-axis and y-axis
+# Line chart for all New York cases of COVID-19
+# Figure and plotspace size
+fig, ax = plt.subplots(figsize=(10, 5))
+# x-axis and y-axis
 ax.plot(new_york.index.values,
         new_york['cases'],
         color='purple')
-# Set title and labels for axes
+# Title and labels for axes
 ax.set(xlabel="Date",
        ylabel="Cases",
        title="Cases of COVID-19 in New York")
+# Show the chart
 plt.show()
 
+# Creating the all new cases chart
+new_york_new_cases = new_york['cases'] - new_york['cases'].shift()
+
+# Line chart for all New York new cases of COVID-19
+# Figure and plotspace size
+fig, ax = plt.subplots(figsize=(10, 5))
+# x-axis and y-axis
+ax.plot(new_york_new_cases.index.values,
+        new_york_new_cases,
+        color='purple')
+# Title and labels for axes
+ax.set(xlabel="Date",
+       ylabel="Cases",
+       title="New Cases of COVID-19 in New York")
+# Show the chart
+plt.show()
+
+# Filtering the last 10 days recorded in New York
 last_ten_ny = new_york.tail(10)
 last_ten_ny
 
-# histogram for last 10 days in New York
+# Line chart for last 10 days of COVID-19 recorded in New York
 # Create figure and plot space
-fig, ax = plt.subplots(figsize=(10, 10))
-# Add x-axis and y-axis
+fig, ax = plt.subplots(figsize=(10, 5))
+# x-axis and y-axis
 ax.plot(last_ten_ny.index.values,
         last_ten_ny['cases'],
         color='purple')
-# Set title and labels for axes
+# Title and labels for axes
 ax.set(xlabel="Date",
        ylabel="Cases",
-       title="Cases of COVID-19 in New York")
+       title="Recent 10 Day All Recorded Cases of COVID-19 in New York")
+# Show the chart
+plt.show()
+
+# Creating the last 10 days new cases chart
+ny_top_ten_new_cases = last_ten_ny['cases'] - last_ten_ny['cases'].shift()
+
+# Line chart for last 10 days of new COVID-19 cases recorded in New York
+# Figure and plotspace size
+fig, ax = plt.subplots(figsize=(10, 5))
+# x-axis and y-axis
+ax.plot(ny_top_ten_new_cases.index.values,
+        ny_top_ten_new_cases,
+        color='purple')
+# Title and labels for axes
+ax.set(xlabel="Date",
+       ylabel="Cases",
+       title="New Cases of COVID-19 in New York")
+# Show the chart
 plt.show()
